4e17500b8ea62de497a8f8464f1e8003ecdcbe66
pasha4uu <pasha7864u@gmail.com>
2019-01-07 22:00:42 +0530

    Initial Commit

---
+++ ToastMessageTests/ToastMessageTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToastMessageTests.swift
+//  ToastMessageTests
+//
+//  Created by PASHA on 07/01/19.
+//  Copyright © 2019 Reatchall. All rights reserved.
+//
+
+import XCTest
+@testable import ToastMessage
+
+class ToastMessageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
